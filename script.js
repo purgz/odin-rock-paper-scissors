ad113977e3fd4376a60566b1c4aa1c5dfa71e433
henryPC <henbrooks6@gmail.com>
2022-04-23 22:28:32 +0100

    rock paper scissors ui added

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let computerOptions = ["Rock","Paper","Scissors"];
+let playerScore = 0;
+let computerScore = 0;
 
+const resultDiv = document.querySelector('#result');
+const playerScoreDiv = document.querySelector('#playerScore');
+const computerScoreDiv = document.querySelector('#computerScore');
+const WINNING_SCORE = 5;
 
 function computerPlay(){
     return computerOptions[Math.floor(Math.random() * computerOptions.length)]
@@ -9,29 +15,53 @@ function playRound(playerSelection, computerSelection){
     //convert input to lower case
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
+
+    
    
-    if ((playerSelection == "rock" && computerSelection == "scissors")
-        || (playerSelection == "scissors" && computerSelection == "paper")
-        || (playerSelection == "paper" && computerSelection == "rock")){
-            return `You win! ${playerSelection} beats ${computerSelection}`;
+    if ((playerSelection == "rock" && computerSelection == "scissors") 
+            || (playerSelection == "scissors" && computerSelection == "paper")
+            || (playerSelection == "paper" && computerSelection == "rock")){
+        
+        resultDiv.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+        playerScore++;
 
     } else if (playerSelection == computerSelection){
-        return `Draw both players chose ${playerSelection}`
+        resultDiv.textContent = `Draw both players chose ${playerSelection}`
         
     } else {
-        return `You lose ${computerSelection} beats ${playerSelection}`
+        resultDiv.textContent = `You lose ${computerSelection} beats ${playerSelection}`
+        computerScore++;
+    }
+
+    playerScoreDiv.textContent = `Player score: ${playerScore}`;
+    computerScoreDiv.textContent = `Computer score: ${computerScore}`;
+
+    if (playerScore == WINNING_SCORE){
+        resultDiv.textContent = "You win";
+        gameOver();
+    } else if (computerScore == WINNING_SCORE){
+        resultDiv.textContent = "You lose";
+        gameOver();
     }
 }
 
-function game(){
-    for (let i = 0; i < 5; i++){
-        let playerSelection = prompt("Enter your choice (rock,paper,scissors");
-        let computerSelection = computerPlay();
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach((button)=>{
+    button.addEventListener('click',clickBtn)
+})
 
-        console.log(playRound(playerSelection,computerSelection))
+function clickBtn(e){
+    //fix
+    let playerChoice = e.target.id;
+   
+    let computerChoice = computerPlay();
 
-    }
+    playRound(playerChoice,computerChoice);
 }
 
 
-game()
+function gameOver(){
+    buttons.forEach((button)=>{
+        button.removeEventListener('click',clickBtn);
+    })
+}
